24d2f9dcc2f45156ab0ae5026e9d9b3226baa536
Yue Hu <yue.hu@vanderbilt.edu>
2020-03-30 20:11:33 -0500

    initial commit

---
+++ js/sidebar.js
@@ -0,0 +1,17 @@
+document.write('<a href="#" class="js-colorlib-nav-toggle colorlib-nav-toggle"><i></i></a>\
+		<aside id="colorlib-aside" role="complementary" class="js-fullheight text-center">\
+			<h1 id="colorlib-logo"><a href="index.html">Hotzword<span>.</span> Food Blog </a> <a href="index.html"><span>“好吃娃”</span> </a></h1>\
+			<nav id="colorlib-main-menu" role="navigation">\
+				<ul>\
+					<li class="colorlib-active"><a href="index.html">Recipe</a></li>\
+					<li><a href="restaurant.html">Restaurant</a></li>\
+					<li><a href="about.html">About</a></li>\
+			</nav>\
+			<div class="colorlib-footer">\
+				<p> For more about me, welcom to my <a href="https://yuehu9.github.io">personal website</a> </p>\
+				<ul>\
+					<li><a href="https://www.instagram.com/yue69999"><i class="icon-instagram"></i></a></li>\
+					</ul>\
+			</div>\
+		</aside> <!-- END COLORLIB-ASIDE -->\
+	');
